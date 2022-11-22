aece5126cd48968a604d04d388ded89c575bc414
egork <egorka.voitenko@gmail.com>
2022-11-22 14:23:24 +0200

    [main] Updated fibonacci check value

--- manager/app_manager.py
+++ manager/app_manager.py
@@ -5,18 +5,34 @@ from manager.static import FILE_PATH, TYPE_ERROR, VALUE_ERROR, ERROR
 
 class Manager:
     @staticmethod
-    def calculate_perfect_square(number: int):
-        seq = int(number**0.5)
-        return seq * seq == number
+    def check_number_for_fibonacci_sequence(number: int) -> bool:
+        if number == 0 or number == 1:
+            return True
+
+        a, result = 0, 1
+        while result < number:
+            b = a + result
+            a, result = result, b
+        if result == number:
+            return True
+        else:
+            return False
 
     @staticmethod
-    def check_value_for_fibonacci_belonging(number: int) -> int:
-        constant_calculation = number**2 * 5
-        return Manager.calculate_perfect_square(constant_calculation + 4) \
-               or Manager.calculate_perfect_square(constant_calculation - 4)
+    def get_reversed_string(string: str) -> str:
+        string_by_symbols = list(string)
+        for index in range(len(string) // 2):
+            current_letter_storage = string_by_symbols[index]
+
+            # start letter replaced to last letter
+            string_by_symbols[index] = string_by_symbols[len(string) - index - 1]
+
+            # reverse replacing - last letter replaced to start letter
+            string_by_symbols[len(string) - index - 1] = current_letter_storage
+        return ''.join(string_by_symbols)
 
     @staticmethod
-    def get_result_by_txt_file(array):
+    def get_result_by_txt_file(array: list):
         logging.warning(f"Path for file storage: {FILE_PATH} in 'get_result_by_txt_file' method")
         with open(FILE_PATH, "w") as file:
             for i in array:
@@ -37,10 +53,13 @@ class Manager:
         for i in range(1, len(array) + 1):
             logging.warning(f'Id number: {i} | '
                             f'value: {array[i-1]} | '
-                            f'reverse value: {Manager.check_value_for_fibonacci_belonging(i)} | '
                             f'len array: {len(array)}')
-            if Manager.check_value_for_fibonacci_belonging(i):
-                result_array.append(array[i-1][::-1])
+
+            if not isinstance(array[i - 1], str):
+                return {f"{ERROR}": f'Value should be string, not {type(array[i - 1])}'}
+
+            if Manager.check_number_for_fibonacci_sequence(i):
+                result_array.append(Manager.get_reversed_string(array[i-1]))
             else:
                 result_array.append(array[i-1])
             Manager.get_result_by_txt_file(result_array)
